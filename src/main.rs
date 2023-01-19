0e8abd11243d981cbb6108a877a0e6413fdcc33a
Victor Tozzatto <victor.tozzatto@gmail.com>
2023-01-19 18:56:45 +0000

    Adding Rocket crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::routes;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, World!"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await.expect("TODO: panic message");
 }
